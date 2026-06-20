6d20d3baf1ee48e72e4fc04d489fdf23e10332bb
monicaakumu <moni.akumu@gmail.com>
2026-06-20 19:08:50 -0400

    Save uploaded files to uploads directory

--- backend/app/upload.py
+++ backend/app/upload.py
@@ -1,12 +1,20 @@
 from fastapi import APIRouter, UploadFile, File
+import shutil
 
 router = APIRouter()
 
 
 @router.post("/upload-label")
 async def upload_label(file: UploadFile = File(...)):
+
+    file_path = f"uploads/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
     return {
         "filename": file.filename,
         "content_type": file.content_type,
+        "saved_to": file_path,
         "status": "uploaded successfully"
     }
